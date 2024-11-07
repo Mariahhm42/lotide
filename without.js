938af62b136663ee71911956bfec74245249454d
Mariahhm42 <134189527+Mariahhm42@users.noreply.github.com>
2024-11-07 13:41:44 -0700

    Update without.js

--- without.js
+++ without.js
@@ -40,3 +40,8 @@ const without = function(source, itemsToRemove) {
 
 assertArraysEqual(without([1, 2, 3], [1]), [2, 3]);
 assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]);
+//test
+
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); 
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
