ef7f60008276be296ac239242fbe254cbe2fba44
Mariahhm42 <134189527+Mariahhm42@users.noreply.github.com>
2024-11-07 19:42:15 -0700

    Update middle.js

--- middle.js
+++ middle.js
@@ -38,4 +38,6 @@ const middle = function(array) {
   // Test cases
   assertArraysEqual(middle([1]), []);
   assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); 
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); 
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+assertArraysEqual(middle([1, 2]), []); 
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); 
