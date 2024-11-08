2e37db510cb9220433fe8e73f0f9b9ca76f4045a
Mariahhm42 <134189527+Mariahhm42@users.noreply.github.com>
2024-11-08 15:03:49 -0700

    Update countOnly.js

--- countOnly.js
+++ countOnly.js
@@ -9,13 +9,18 @@ const assertEqual = function(actual, expected) {
 //count only function 
 const countOnly = function(allItems, itemsToCount) {
     const results = {};
-  
     for (const item of allItems) {
-        console.log(item);
+      if (itemsToCount[item]) {
+        if (results[item]) {
+          results[item] += 1;
+        } else {
+          results[item] = 1;
+        }
       }
-    
-      return results;
-    };
+    }
+    return results;
+  };
+
   const firstNames = [
     "Karl",
     "Salima",
