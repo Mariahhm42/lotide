d039675e9bd2e10f4499e623644f6fe90a7c3783
Mariam Agboola <mariahhm42@gmail.com>
2024-11-09 10:35:01 -0700

    refactoring

--- head.js
+++ head.js
@@ -1,17 +1,16 @@
 // function implementation of the assertEqual.js
-const assertEqual = function(actual, expected) {
-    if (actual === expected) {
-        console.log(`✅ Assertion Passed: ${actual}===${expected}`);
-    } else {
-        console.log(`❌ Assertion Failed ${actual} !== ${expected}`)
-    }
-};
+// const assertEqual = function(actual, expected) {
+//     if (actual === expected) {
+//         console.log(`✅ Assertion Passed: ${actual}===${expected}`);
+//     } else {
+//         console.log(`❌ Assertion Failed ${actual} !== ${expected}`)
+//     }
+// };
+const assertEqual = require("./assertEqual");
 
 //Head function 
 const head = function(array) {
     return array[0];
 };
-
+module.exports = head;
 // TEST CODE
-assertEqual(head(["Lighthouse Labs", "Bootcamp"]), "Mariam" );
-assertEqual(head([1]), 1);
