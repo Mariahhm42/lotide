99f6d62227237d257aa0bd5fb245e886667a7611
Mariam Agboola <mariahhm42@gmail.com>
2024-11-15 18:00:40 -0700

    edited index.js

--- index.js
+++ index.js
@@ -2,10 +2,50 @@
 const head   = require('./head');
 const tail   = require('./tail');
 const middle = require('./middle');
+const assertArrasyEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+const countLetters = require('./countLetters');
+const flatten = require('./flatten');
+const map = require('./map');
+const eqObjects = require('./eqObjects');
+const assertObjectEqual = require('./assertObjectEqual');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+const countOnly = require('./countOnly');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const letterPositions = require('./letterPositions');
+const assertArraysEqualTest = require('./assertArraysEqualTest');
+const assertEqualTest = require('./assertEqualTest');
+const eqArraysTest = require('./eqArraysTest');
+const headTest = require('./headTest');
+const middleTest = require('./middleTest');
+const tailTest = require('./tailTest');
 
 //export the functions using shorthand property names
 module.exports = {
+  assertArraysEqual,
+  assertEqual,
+  countLetters,
+  eqArrays,
+  flatten,
+  map,
   head,
   tail,
-  middle
+  middle,
+  eqObjects,
+  assertObjectEqual,
+  takeUntil,
+  without,
+  countOnly,
+  findKey,
+  findKeyByValue,
+  letterPositions,
+  assertArraysEqualTest,
+  assertEqualTest,
+  eqArraysTest,
+  headTest,
+  middleTest,
+  tailTest,
+
 };
